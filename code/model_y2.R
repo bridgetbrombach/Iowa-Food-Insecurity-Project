0b628ec6445e0370cbb23ce4d60b6c26f07697ff
bridgetbrombach <127122279+bridgetbrombach@users.noreply.github.com>
2024-12-04 11:37:08 -0600

    updated model with Random Forest models

--- code/model_y2.R
+++ code/model_y2.R
@@ -85,3 +85,85 @@ acs.preds <- acs_data %>%
   mutate(
     lasso_pred = predict(lasso, acs_data_predict, type="response"),
   )
+
+
+
+############RANDOM FOREST FSBAL##############
+#split data in to train and test
+
+RNGkind(sample.kind = "default")
+train.idx <- sample(x = 1:nrow(cps_data), size = .7*nrow(cps_data))
+train.df <- cps_data[train.idx,]
+test.df <- cps_data[-train.idx,]
+
+
+#Fit a baseline forest 
+
+
+tempforest <- randomForest(as.factor(FSBAL) ~ hhsize+education+femhispanic+femblack+poverty+donutfamily+hispanic+married+female+
+                             elderly, 
+                           data = train.df,
+                           ntree = 100,
+                           mtry = 4,
+                           weights = train.df$weight)
+
+
+dim(train.df)
+
+mtry <- seq(from = 1, to = 8, by = 3)
+
+keeps <- data.frame(m = rep(NA, length(mtry)),
+                    OOB_err_rate = rep(NA, length(mtry)))
+
+for(idx in 1:length(mtry)){
+  print(paste0("Trying m = ", mtry[idx]))
+  
+  tempforest <- randomForest(as.factor(FSBAL) ~ hhsize+education+femhispanic+femblack+poverty+donutfamily+hispanic+married+female+
+                               elderly, 
+                             data = train.df,
+                             ntree = 1000,
+                             mtry = mtry[idx],
+                             weights = train.df$weight)
+  
+  keeps[idx, "m"] <- mtry[idx]
+  
+  keeps[idx, "OOB_err_rate"] <- mean(predict(tempforest) != train.df$FSBAL)
+}
+
+ggplot(data = keeps) +
+  geom_line(aes(x = m, y = OOB_err_rate))+
+  theme_bw() + labs(x = "m (mtry value)", y = "OOB Error Rate(minimize)") +
+  scale_x_continuous(breaks = c(1:19))
+
+# 4 looks to be the optimal number the m that minimizes the 
+# OOB Error Rate
+
+finalforest <- randomForest(as.factor(FSBAL) ~ hhsize+education+femhispanic+femblack+poverty+donutfamily+hispanic+married+female+
+                              elderly, 
+                            data = train.df,
+                            ntree = 1000,
+                            mtry = 4,  #chosen from tuning
+                            weights = train.df$weight, 
+                            importance = TRUE)
+
+pi_hat <- predict(finalforest, test.df, type = "prob")[,"1"]
+
+
+
+rocCurve <- roc(response = test.df$FSBAL,
+                predictor = pi_hat,
+                levels = c("0","1"))
+
+
+plot(rocCurve, print.thres = TRUE, print.auc = TRUE) 
+
+
+
+
+
+
+
+
+
+
+
