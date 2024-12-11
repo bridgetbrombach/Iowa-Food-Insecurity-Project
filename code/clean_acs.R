5086f5d2be5309b5c5ba7712257b43d17cb95657
ahana yelagar <ahanayelagar2@gmail.com>
2024-12-11 12:18:19 -0600

    uploaded image

--- code/clean_acs.R
+++ code/clean_acs.R
@@ -9,19 +9,16 @@ acs <- acs%>%
 acs <- acs %>%
   mutate(SEX = Sex - 1 , # since female = 2
          CHILD = ifelse(Age < 18, 1, 0), #SAME as cps definition
-<<<<<<< Updated upstream
          WORKING=ifelse(Age > 17 & Age <60, 1, 0),
          ELDERLY = ifelse(Age > 59, 1, 0), #SAME as cps definition
          ASIAN=ifelse(Race==1,1,0),#SAME as cps definition
          BLACK = ifelse(Race==2, 1, 0), #SAME as cps definition (see data dictionary) 
          HISPANIC = ifelse(Hispanic>0, 1, 0), #SAME as cps definition (see data dictionary) 
          EDUC = as.integer(Education %in% c(3,4)), #above college level education
-=======
          ELDERLY = ifelse(Age > 64, 1, 0), #SAME as cps definition
          BLACK = ifelse(Race==2, 1, 0), #SAME as cps definition (see data dictionary) 
          HISPANIC = ifelse(Hispanic>0, 1, 0), #SAME as cps definition (see data dictionary 
          EDUC = as.integer(Education %in% c(3,4)),
->>>>>>> Stashed changes
          MARRIED = as.integer(Mar %in% c(1)),
          poverty=OFFPoor,
          ELMAR = ELDERLY*MARRIED,
